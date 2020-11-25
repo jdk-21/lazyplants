044cd84216c5047b5766fa9bb9e986186c844e46
root <root@vmd61731.contaboserver.net>
2020-11-25 11:39:15 +0100

    new database

--- common/models/plants.js
+++ common/models/plants.js
@@ -19,4 +19,23 @@ module.exports = function(Plants) {
         "http": {"verb": "get", "path": "/allMyPlants"},
     });
 
+
+    Plants.MyPlants = function async (id, callback) {
+        
+        //var userId = req.accessToken.userId;
+        var filter = {"where": {"id": id}};
+        Plants.find(Plants.allMyPlants() & filter, function (err, Plants) {
+          if (err !== null) {
+            callback(err);
+          }
+          callback(null, Plants);
+        });
+    }
+
+    Plants.remoteMethod('MyPlants', {
+        "accepts": { arg: 'id',arg: 'req', type: 'string', arg: 'req', type: 'object', http: {source: 'req'}},
+        "returns": { arg: 'MyPlants', type: 'Plants' },
+        "http": {"verb": "get", "path": "/MyPlants"},
+    });
+
 };
