166e0210987e3472a047c96164bfe992dc0a4d30
root <root@vmd61731.contaboserver.net>
2020-12-08 13:52:39 +0100

    added allMyData

--- common/models/plant-data.js
+++ common/models/plant-data.js
@@ -1,5 +1,22 @@
 'use strict';
 
-module.exports = function(Plantdata) {
+module.exports = function(PlantData) {
+
+    PlantData.allMyData = function (req, callback) {
+        var userId = req.accessToken.userId;
+        var filter = {"where":{"memberId": userId}};
+        PlantData.find(filter, function (err, PlantData) {
+          if (err !== null) {
+            callback(err);
+          }
+          callback(null, PlantData);
+        });
+    }
+
+    PlantData.remoteMethod('allMyData', {
+        "accepts": { arg: 'req', type: 'object', http: {source: 'req'}},
+        "returns": { arg: 'allMyData', type: 'plant-data' },
+        "http": {"verb": "get", "path": "/allMyData"},
+    });
 
 };
