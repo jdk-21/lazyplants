6d756ff7ac632218da7901304c71e1e096b38993
Patrick Luber <patrickluber7@gmail.com>
2021-05-14 15:19:23 +0200

    1. Versuch

--- lib/components/api_connector2.dart
+++ lib/components/api_connector2.dart
@@ -23,6 +23,7 @@ var a = new Member;
 a.request(); //Standart request (get)
 a.setRequest(new Post); // Umschalten auf Post
 a.request(); //post
+a.login.request
 */
 abstract class ApiMember extends ApiConnector {
 //Post Plant wird nicht verwendet
@@ -32,18 +33,23 @@ abstract class ApiMember extends ApiConnector {
   String lastName;
   String userName;
   String userId;
-  ApiMember(String mail, String pw, String firstName, String lastName,
-      String userName, String userId) {
+  ApiMember.createAccount(String mail, String pw, String firstName,
+      String lastName, String userName) {
     this.mail = mail;
     this.pw = pw;
     this.firstName = firstName;
     this.lastName = lastName;
     this.userName = userName;
+  }
+  ApiMember.memberData(this.userId) {
     this.userId = userId;
   }
-  ApiMember.getMember(this.userId) {
-    this.userId = userId;
+  ApiMember.login(this.mail, this.pw) {
+    this.mail = mail;
+    this.pw = pw;
   }
+  ApiMember.logout();
+
   var uri = Uri.parse(ApiConnector.baseUrl + "Members");
 
   RequestVerhalten requestVerhalten = new GetRequest();
@@ -51,39 +57,130 @@ abstract class ApiMember extends ApiConnector {
     this.requestVerhalten = requestVerhalten;
   }
 
-  Future loginRequest() {
-    uri = Uri.parse(uri.toString() + "/login");
-    var body = jsonDecode('{"email": $mail, "password": $pw}');
-    return requestVerhalten.request(uri, body);
-  }
+  Future request() async {}
+}
 
-  Future logoutRequest() {
+class Login extends ApiMember {
+  Login(String mail, String pw) : super.login(mail, pw);
+  Future request() async {
+    var uri = Uri.parse(ApiConnector.baseUrl + "Members/login");
+    RequestVerhalten rv = requestVerhalten;
+    var body = jsonDecode('{"email": $mail, "password": $pw}');
+    var response = requestVerhalten.request(uri, body);
+    if (response.statusCode == 200) {
+      print("ok");
+      var data = await jsonDecode(response.body);
+      settingsBox.put("token", data["id"]);
+      settingsBox.put("userId", data['userId']);
+      userId = data['userId'];
+      setRequest(GetRequest());
+      await request(); //first- and lastName to DB
+      setRequest(rv);
+      return 0;
+    } else {
+      print('error: ' + response.statusCode.toString());
+      return 1;
+    }
+  }
+}
+
+class Logout extends ApiMember {
+  Logout() : super.logout();
+  Future request() async {
     uri = Uri.parse(
         uri.toString() + "/logout?access_token=" + settingsBox.get('token'));
     var body = "";
     return requestVerhalten.request(uri, body);
   }
+}
 
-  Future request() {
-    if (requestVerhalten is GetRequest) {
-      uri = Uri.parse(uri.toString() +
-          "/" +
-          userId +
-          "?access_token=" +
-          settingsBox.get('token'));
-    }
+class CrateAccount extends ApiMember {
+  CrateAccount(String mail, String pw, String firstName, String lastName,
+      String userName)
+      : super.createAccount(mail, pw, firstName, lastName, userName);
+  Future request() async {
     var body = jsonDecode('''{
         "firstname": $firstName,
         "lastname": $lastName,
-        "username": $username,
+        "username": $userName,
         "email": $mail,
         "password": $pw
       }''');
-    return requestVerhalten.request(uri, body);
+    var response = requestVerhalten.request(uri, body);
+    var data = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      settingsBox.put('userId', data['id']);
+      settingsBox.put('firstName', data['firstname']);
+      settingsBox.put('lastName', data['lastname']);
+      Login login = Login(mail, pw);
+      if (await login.request() != 0) return 4;
+      return 0;
+    } else if (response.statusCode == 422) {
+      bool usernameExists;
+      bool emailExists;
+      try {
+        emailExists = data['error']['details']['messages']['email'][0] ==
+            "Email already exists";
+      } catch (e) {
+        emailExists = false;
+      }
+      try {
+        usernameExists = data['error']['details']['messages']['username'][0] ==
+            "User already exists";
+      } catch (e) {
+        usernameExists = false;
+      }
+      if (emailExists && usernameExists)
+        return 1;
+      else if (emailExists)
+        return 2;
+      else if (usernameExists) return 3;
+      return 0;
+    } else {
+      print(response.statusCode.toString());
+      print('error');
+      return 4;
+    }
   }
 }
 
-abstract class ApiData extends ApiConnector {
+class MemberData extends ApiMember {
+  MemberData(String userId) : super.memberData(userId);
+  Future request() async {
+    try {
+      var response = await client.get(Uri.parse(ApiConnector.baseUrl +
+          "Members/" +
+          userId +
+          "?access_token=" +
+          settingsBox.get('token')));
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        settingsBox.put('firstName', data['firstname']);
+        settingsBox.put('lastName', data['lastname']);
+        print("got Members data");
+        return 0;
+      } else if (response.statusCode == 401) {
+        // show login screen
+        print(response.statusCode);
+        return 1;
+      } else
+        print(response.statusCode);
+      return 1;
+    } catch (socketException) {
+      print('No internet connection');
+      return 1;
+    }
+  }
+
+  checkLoggedIn() {
+    if (settingsBox.get("token") != null)
+      return true;
+    else
+      return false;
+  }
+}
+
+abstract class ApiPlantData extends ApiConnector {
   //Patch und Post wird nicht verwendet
   var uri = Uri.parse(ApiConnector.baseUrl +
       "Plants?access_token=" +
