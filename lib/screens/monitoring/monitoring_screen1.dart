7dd1e1bd70560acfabca08ba7ca6b60b59f9e571
Patrick Luber <patrickluber7@gmail.com>
2021-05-21 12:30:09 +0200

    Init

---
+++ lib/screens/monitoring/monitoring_screen1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Monitoring_screen1 extends StatelessWidget {
+  Monitoring_screen1({
+    Key key,
+  }) : super(key: key);
+
+  final Plant plant = Plant();
+
+  @Override
+  Widget @override
+  Widget build(BuildContext context) {
+    return ;
+  }
