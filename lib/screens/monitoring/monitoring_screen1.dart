58fe15154a30320bafc33fa2de6459733a608394
Patrick Luber <patrickluber7@gmail.com>
2021-05-25 10:02:06 +0200

    Verlinkung der Monitoring-Seite

--- lib/screens/monitoring/monitoring_screen1.dart
+++ lib/screens/monitoring/monitoring_screen1.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:lazyplants/components/db_models.dart';
 
-class Monitoring_screen1 extends StatelessWidget {
-  Monitoring_screen1({
+class MonitoringScreen1 extends StatelessWidget {
+  final Plant plant;
+  MonitoringScreen1({
     Key key,
+    @required this.plant,
   }) : super(key: key);
 
-  final Plant plant = Plant();
-
-  @Override
-  Widget @override
+  @override
   Widget build(BuildContext context) {
-    return ;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Monitoring'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(
+                color: Colors.red,
+                height: 200,
+              ),
+              Container(color: Colors.blue, height: 200),
+            ],
+          ),
+        ));
   }
+}
+
+/*Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MonitoringScreen1(
+                              plant: widget.plant,
+                            )),
+                  );*/
