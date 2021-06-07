9598b513e165f92f600327bef88ddd8638546b85
Patrick Luber <patrickluber7@gmail.com>
2021-06-07 18:56:33 +0200

    Implementieren des Drop-Down-Menüs

--- lib/screens/monitoring/monitoring_screen1.dart
+++ lib/screens/monitoring/monitoring_screen1.dart
@@ -5,34 +5,65 @@ import 'package:lazyplants/components/db_models.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:intl/intl.dart'; // für primaryYAxis
 import 'package:intl/date_symbol_data_file.dart';
+//initializeDateFormatting('de_DE', null);
 
-class MonitoringScreen1 extends StatelessWidget {
+class MonitoringScreen1 extends StatefulWidget {
   final Plant plant;
   MonitoringScreen1({
     Key key,
     @required this.plant,
   }) : super(key: key);
-  TooltipBehavior _tooltipBehavior =
-      TooltipBehavior(enable: true); // Anzeige der Datenpunkte beim Anklicken
+
+  @override
+  _MonitoringScreen1State createState() => _MonitoringScreen1State();
+}
+
+class _MonitoringScreen1State extends State<MonitoringScreen1> {
+  TooltipBehavior _tooltipBehavior = TooltipBehavior(enable: true);
   int limit = 5;
+
   int selectedValue = 1;
-  String wert = "temperature";
+
+  String wert;
+
+  var textStyle =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black);
 
   @override
   Widget build(BuildContext context) {
-    initializeDateFormatting('de_DE', null);
-    if (selectedValue == 1) {
-      wert = "temperature";
-    }
-    if (selectedValue == 2) {
-      wert = "huminity";
+    switch (selectedValue) {
+      case 1:
+        wert = "temperature";
+        break;
+      case 2:
+        wert = "humidity";
+        break;
+      case 3:
+        wert = "soilMoisture";
+        break;
+      case 4:
+        wert = "tanklevel";
+        break;
+      default:
+        wert = "temperature";
     }
     return FutureBuilder(
-        future: api.getExactPlantData(limit, plant.plantId),
+        future: api.getExactPlantData(limit, widget.plant.plantId),
         builder: (BuildContext context, AsyncSnapshot<dynamic> plantData) {
           if (!plantData.hasData) {
-            return CircularProgressIndicator();
+            return Scaffold(
+                body: Container(child: CircularProgressIndicator()));
+          } else if (plantData.data is String) {
+            return Scaffold(
+                backgroundColor: Colors.red[800],
+                body: Center(
+                    child: Text(plantData.data.toString(),
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black))));
           }
+
           var mittelwert = 0.0;
           var count = 0;
 
@@ -47,35 +78,71 @@ class MonitoringScreen1 extends StatelessWidget {
               appBar: AppBar(
                 title: Text('Monitoring'),
               ),
-              body: Container(
+              body: ListView(
                 padding: EdgeInsets.all(5),
-                child: Column(
-                  children: [
-                    CustomDiagram(
-                        plant: plant,
+                children: [
+                  Center(
+                      child: Text(widget.plant.plantName,
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ))),
+                  SizedBox(height: 10),
+                  Container(
+                    height: 300,
+                    child: CustomDiagram(
+                        plant: widget.plant,
                         limit: limit,
                         plantData: plantData.data,
                         wert: wert,
                         tooltipBehavior: _tooltipBehavior),
-                    SizedBox(height: 5), // Abstand von 5zwischen Containern
-                    DropDownExample(),
-                    Container(
-                        //color: Colors.blue,
-                        height: 200,
-                        child: Column(
-                          //mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "letzter Messzeitpunkt: ${measuringTime.toString()}\nMittelwert: $mittelwert \n",
+                  ),
+                  SizedBox(height: 5), // Abstand von 5zwischen Containern
+                  Container(
+                    height: 100,
+                    child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Center(
+                          child: DropdownButton(
+                              underline: Container(
+                                  height: 0, color: CustomColors.kPrimaryColor),
+                              iconDisabledColor: CustomColors.kPrimaryColor,
                               style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),
-                            ),
-                          ],
+                                  color: CustomColors.kPrimaryColor,
+                                  fontWeight: FontWeight.bold),
+                              dropdownColor: Colors.green[200],
+                              value: selectedValue,
+                              items: [
+                                DropdownMenuItem(
+                                    child: Text("Temperatur"), value: 1),
+                                DropdownMenuItem(
+                                    child: Text("Luftfeuchtigkeit"), value: 2),
+                                DropdownMenuItem(
+                                    child: Text("Bodenfeuchtigkeit"), value: 3),
+                                DropdownMenuItem(
+                                    child: Text("Tank Level"), value: 4),
+                              ],
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedValue = value;
+                                });
+                              }),
                         )),
-                  ],
-                ),
+                  ),
+                  Container(
+                      //color: Colors.blue,
+                      height: 200,
+                      child: Column(
+                        //mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                              "letzter Messzeitpunkt: ${measuringTime.toString()} \n",
+                              style: textStyle),
+                          Text("Mittelwert: $mittelwert", style: textStyle),
+                        ],
+                      )),
+                ],
               ));
         });
   }
@@ -136,47 +203,5 @@ class CustomDiagram extends StatelessWidget {
             )
       ],
     ));
-  }
-}
-
-/////////////////////////////////////////////////////////////////////////////////////
-class DropDownExample extends StatefulWidget {
-  DropDownExample({Key key}) : super(key: key);
-
-  @override
-  _DropDownExampleState createState() => _DropDownExampleState();
-}
-
-class _DropDownExampleState extends State<DropDownExample> {
-  int selectedValue = 1;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Center(
-              child: DropdownButton(
-                  value: selectedValue,
-                  items: [
-                    DropdownMenuItem(
-                      child: Text("Male"),
-                      value: 1,
-                    ),
-                    DropdownMenuItem(
-                      child: Text("Female"),
-                      value: 2,
-                    ),
-                    DropdownMenuItem(child: Text("Others"), value: 3),
-                  ],
-                  onChanged: (value) {
-                    setState(() {
-                      selectedValue = value;
-                    });
-                  }),
-            )),
-      ),
-    );
   }
 }
